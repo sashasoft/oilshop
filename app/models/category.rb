711a433dd3fd3bb9f1bc63c3f300d4678378fd9b
sashasoft <kovalenko.alexandrs@gmail.com>
2015-01-26 17:29:23 +0200

    add model Subcategory and controller Subcategories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   belongs_to :brand
+  has_many :subcategories
   validates :name, presence: true, length: { minimum: 8 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124192038) do
+ActiveRecord::Schema.define(version: 20150126131914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,15 @@ ActiveRecord::Schema.define(version: 20150124192038) do
   end
 
   add_index "categories", ["brand_id"], name: "index_categories_on_brand_id", using: :btree
+
+  create_table "subcategories", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "subcategories", ["category_id"], name: "index_subcategories_on_category_id", using: :btree
 
 end
